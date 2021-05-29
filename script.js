88a4d48cfe129dcbaffb1dc2f384c36443cb7ff8
James Hooper <jameshooperdesigns@gmail.com>
2021-05-29 00:27:11 -0600

    Fix animation and make changes to the script file

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-/* This script animates the text */
-
 const quotes = document.querySelectorAll(".quote");
 
 function setupSplits() {
@@ -10,21 +8,21 @@ function setupSplits() {
       quote.split.revert();
     }
 
-    quote.split = new SplitText(quote, {
+    quote.split = new SplitText(quote, { 
       type: "lines,words,chars",
       linesClass: "split-line"
     });
 
     // Set up the anim
-    quote.anim = gsap.from(quote,split.chars, {
+    quote.anim = gsap.from(quote.split.chars, {
       scrollTrigger: {
         trigger: quote,
         toggleActions: "restart pause resume reverse",
         start: "top 50%",
       },
-      duration: 0.6,
-      ease: "circ.out",
-      y: 80,
+      duration: 0.6, 
+      ease: "circ.out", 
+      y: 80, 
       stagger: 0.02,
     });
   });
